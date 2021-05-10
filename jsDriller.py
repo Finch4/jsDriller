e53e8ddde9db9a4535e9c000eca311874ba9f201
Finch <52568048+Finch4@users.noreply.github.com>
2021-05-10 10:01:47 +0200

    Update jsDriller.py

--- jsDriller.py
+++ jsDriller.py
@@ -1,12 +1,12 @@
 import re
 import base64
-
-
+import jsbeautifier
 
 
 
 def return_function_name(function_content):
     return str(re.findall("(\\w+)\\(.*?\\)\\{", function_content))
+
 
 file_name = "test.txt"
 file = open(file_name, "r").read()
@@ -25,36 +25,47 @@ strings = re.findall("\'.*?'|\".*?\"", file)
 
 report = {
     "normal_functions": normal_functions,
-    "functions_contents": functions_contents,
-    "functions_inside_dictionaries": functions_inside_dictionaries,
-    "functions_as_variable": functions_as_variable,
-    "if_statements": if_statements,
-    "variables": variables,
-    "try_blocks": try_blocks,
-    "else_statements": else_statements,
+    "functions_contents": jsbeautifier.beautify(str(functions_contents)),
+    "functions_inside_dictionaries": jsbeautifier.beautify(str(functions_inside_dictionaries)),
+    "functions_as_variable": jsbeautifier.beautify(str(functions_as_variable)),
+    "if_statements": jsbeautifier.beautify(str(if_statements)),
+    "variables": set(variables),
+    "try_blocks": jsbeautifier.beautify(str(try_blocks)),
+    "else_statements": jsbeautifier.beautify(str(else_statements)),
     "strings": strings,
+
+    # Not really working the regex
     "base_64_strings": re.findall("(?:[A-Za-z0-9+\\/]{4}\\n?)*(?:[A-Za-z0-9+\\/]{2}==|[A-Za-z0-9+\\/]{3}=)", file),
-    "variables_inside_functions": [],
-    "variables_inside_if": [],
-    "variables_inside_else": [],
-    "variables_inside_try": [],
+
+    "variables_inside_functions": set(),
+    "variables_inside_if": set(),
+    "variables_inside_else": set(),
+    "variables_inside_try": set(),
 }
+
+variable_count = \
+    [
+
+    ]
 
 
 for variable in variables:
     for function_content in functions_contents:
         id = return_function_name(str(function_content))
         if variable in str(function_content):
-            report["variables_inside_functions"].append(
-                f"Variable {variable} found inside {return_function_name(str(function_content))}")
+            report["variables_inside_functions"].add(
+                f"Variable {variable} found inside {return_function_name(str(function_content))}, count: {str(function_content).count(variable)}")
     for if_content in if_statements:
         if variable in if_content:
-            report["variables_inside_if"].append(f"Variable {variable} found inside {if_content}")
+            report["variables_inside_if"].add(f"Variable {variable} found inside {if_content}, count: {str(if_content).count(variable)}")
     for else_content in else_statements:
         if variable in else_content:
-            report["variables_inside_else"].append(f"Variable {variable} found inside {else_content}")
+            report["variables_inside_else"].add(f"Variable {variable} found inside {else_content}, count: {str(else_content).count(variable)}")
     for try_content in try_blocks:
         if variable in try_content:
-            report["variables_inside_try"].append(f"Variable {variable} found inside {try_content}")
+            report["variables_inside_try"].add(f"Variable {variable} found inside {try_content}, , count: {str(try_content).count(variable)}")
 
-open(f"analysis_{file_name}.txt", "w").write(str(report))
+file = open(f"analysis_{file_name}.txt", "a")
+file.write(str(report) + "\n"*10)
+file.write(jsbeautifier.beautify_file(file_name))
+file.close()
